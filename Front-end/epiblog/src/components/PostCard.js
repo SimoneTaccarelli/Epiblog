865be28c3c8cc6b644d4304d2d502ef5ad788aca
Simone <simone-taccarelli@hotmail.it>
2025-04-04 12:24:55 +0200

    aggiornato postcard

--- Front-end/epiblog/src/components/PostCard.js
+++ Front-end/epiblog/src/components/PostCard.js
@@ -38,53 +38,54 @@ const PostCard = ({ post, refreshPosts }) => {
         <>
             <Card key={post._id} className="my-4 w-75 post-card" >
                 <div onClick={handleClickDetails}>
-                <Card.Header className="d-flex justify-content-between p-0">
-                    <Row className="align-items-center">
-                        <div className="col-auto">
-                            <Card.Img
-                                className="ms-3 my-3"
-                                variant="top"
-                                src={post.author?.profilePic || defaultImg}
-                                alt="profile"
-                                style={{ width: '40px', height: '40px', borderRadius: '50%', objectFit: 'cover' }}
-                            />
-                        </div>
-                        <div className="col">
-                            <h5>{post.author?.firstName} {post.author?.lastName}</h5>
-                        </div>
-                    </Row>
-                </Card.Header>
+                    <Card.Header className="d-flex justify-content-between p-0">
+                        <Row className="align-items-center">
+                            <div className="col-auto">
+                                <Card.Img
+                                    className="ms-3 my-3"
+                                    variant="top"
+                                    src={post.author?.profilePic || defaultImg}
+                                    alt="profile"
+                                    style={{ width: '40px', height: '40px', borderRadius: '50%', objectFit: 'cover' }}
+                                />
+                            </div>
+                            <div className="col">
+                                <h5>{post.author?.firstName} {post.author?.lastName}</h5>
+                            </div>
+                        </Row>
+                    </Card.Header>
 
-                <Link to={`/post/${post._id}`} className='link-postDetails my-3 mx-3'>{post.title}</Link>
-                <div
-                    className='post-cover'
-                    style={{
-                        backgroundImage: `url(${post.cover})`
-                    }}
-                >
+                    <Link to={`/post/${post._id}`} className='link-postDetails my-3 mx-3'>{post.title}</Link>
+                    <div
+                        className='post-cover'
+                        style={{
+                            backgroundImage: `url(${post.cover})`
+                        }}
+                    >
+                    </div>
                 </div>
-                <Card.Body>
-                    {user && user._id === post.author._id ? (
-                        <div className="d-flex justify-content-between">
-                            <div>
-                                <span className="badge bg-secondary me-2">{post.category}</span>
+                    <Card.Body>
+                        {user && user._id === post.author._id ? (
+                            <div className="d-flex justify-content-between">
+                                <div>
+                                    <span className="badge bg-secondary me-2">{post.category}</span>
+                                </div>
+                                <div>
+                                    <Button variant='link deletPostBtn' onClick={deletePost}>Delete Post</Button>
+                                    <ModalModifyPost id={post._id} />
+                                </div>
                             </div>
-                            <div>
-                                <Button variant='link deletPostBtn' onClick={deletePost}>Delete Post</Button>
-                                <ModalModifyPost id={post._id} />
-                            </div>
-                        </div>
-                    ) : (
-                        <Card.Subtitle className="mb-2 text-muted">{post.category}</Card.Subtitle>
-                    )}
+                        ) : (
+                            <Card.Subtitle className="mb-2 text-muted">{post.category}</Card.Subtitle>
+                        )}
 
-                    <Card.Text>{post.description}</Card.Text>
-                    <small className="text-muted">Read time: {post.readTime.value} {post.readTime.unit}</small>
-                </Card.Body>
-            </div>
-            <AddComments postId={post._id} />
-        </Card >
-       
+                        <Card.Text>{post.description}</Card.Text>
+                        <small className="text-muted">Read time: {post.readTime.value} {post.readTime.unit}</small>
+                    </Card.Body>
+                
+                <AddComments postId={post._id} />
+            </Card >
+
         </>
     );
 }
